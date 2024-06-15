7be1e3499bdc81789600e120910a8839d2246b30
Will Knight <45063456+williamxb@users.noreply.github.com>
2024-06-15 18:42:14 +0100

    updated nav

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,12 +3,19 @@ import Link from 'next/link'
 const navItems = {
   '/': {
     name: 'Home',
+    ulClass: ''
   },
   '/about': {
     name: 'About',
+    ulClass: ''
   },
-  '/contact': {
-    name: 'Contact',
+  'https://www.linkedin.com/in/emily-willis-646bb2171/': {
+    name: 'LinkedIn',
+    ulClass: 'mt-6'
+  },
+  'mailto:willisjemily@gmail.com': {
+    name: 'Email',
+    ulClass: ''
   }
 }
 
@@ -17,45 +24,26 @@ export function Navbar() {
     <aside className="lg:min-w-96 mb-16 tracking-tight">
       <div className="pr-6 sticky top-8">
         <Link href="/"><h1 className="text-4xl tracking-tight mb-6">Emily Willis</h1></Link>
-        <p className="text-lg mb-6 max-width-96">London-based Graphic Designer, specialised in branding and illustration.</p>
+        <h2 className="text-lg mb-6 max-width-96">London-based Graphic Designer, specialised in branding and illustration.</h2>
         <nav
           className="flex flex-col items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-col space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
+          <ul className="flex flex-col space-x-0 pr-10">
+            {Object.entries(navItems).map(([path, { name, ulClass }]) => {
               return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all text-lg hover:text-hover:text-neutral-800 dark:hover:text-neutral-200 hover:underline flex align-middle relative"
-                >
-                  {name}
-                </Link>
+                <li key={name} className={ulClass}>
+                  <Link
+                    href={path}
+                    className="transition-all text-lg hover:text-hover:text-neutral-800 dark:hover:text-neutral-200 hover:underline flex align-middle relative"
+                  >
+                    {name}
+                  </Link>
+                </li>
               )
             })}
-            <Link
-              className="mt-6 flex items-center transition-all hover:text-hover:text-neutral-800 dark:hover:text-neutral-200 hover:underline"
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://www.linkedin.com/in/emily-willis-646bb2171/"
-            >
-              <p className="mr-2 text-lg">LinkedIn</p>
-            </Link>
-
-            <div className="flex flex-row">
-              <Link
-                className="flex items-center transition-all hover:text-hover:text-neutral-800 dark:hover:text-neutral-200 hover:underline"
-                rel="noopener noreferrer"
-                target="_blank"
-                href="mailto:willisjemily@gmail.com"
-              >
-                <p className="mr-2 text-lg">Email</p>
-              </Link>
-            </div>
-          </div>
+          </ul>
         </nav>
-
       </div>
     </aside>
   )
