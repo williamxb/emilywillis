9f99750eefffe4d3170ec9e4bf168b5dfa6f6c85
Will Knight <45063456+williamxb@users.noreply.github.com>
2024-10-08 01:05:53 +0100

    fixed bugs

--- app/components/homeLink.tsx
+++ app/components/homeLink.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import Image from "next/image"
-import StaticImageData from "next/image"
+import { StaticImageData } from "next/image"
 interface homeLinkProps {
   href: string,
   image: StaticImageData,
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './global.css'
-import Metadata from 'next'
+import { Metadata } from 'next'
 import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
