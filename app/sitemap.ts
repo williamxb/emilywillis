72dce88be2bc85b9ff22106e0ce054e67db1a2e1
Will Knight <45063456+williamxb@users.noreply.github.com>
2025-01-06 23:39:13 +0000

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ export const baseUrl = "https://www.emilywillis.co.uk/";
 export default async function sitemap() {
   let routes = ["", "about", "oasen", "zophos", "keep-dancing", "cautionary-tales-for-children"].map((route) => ({
     url: `${baseUrl}${route}`,
-    lastModified: new Date(1723420800000).toISOString().split("T")[0],
+    lastModified: new Date(1736208000).toISOString().split("T")[0],
   }));
 
   return [...routes];
