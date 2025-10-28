a8a03082740fa53350d0970cb6609dac166bd51f
Will <45063456+williamxb@users.noreply.github.com>
2025-10-28 11:25:52 +0000

    fix: image quality error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
-import type { NextConfig } from 'next'
 import { withNextVideo } from 'next-video/process';
  
-const nextConfig: import('next').NextConfig = {};
+const nextConfig: import('next').NextConfig = {
+    images: {
+        qualities: [50, 75, 100]
+    }
+};
  
 export default withNextVideo(nextConfig);
